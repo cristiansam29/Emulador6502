5a5c3585f0878f670adad1dd06598468822c3007
cristiansam29 <Cristiansam32@gmail.com>
2024-02-13 01:22:49 -0300

    Add files via upload

---
+++ Bus.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+using Addr = uint16_t;
+
+class c6502;
+
+class Bus {
+public:
+    virtual uint8_t read( c6502 *cpu, Addr address, bool sync = false ) = 0;
+    virtual void write( c6502 *cpu, Addr address, uint8_t ) = 0;
+};
